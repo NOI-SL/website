54a947041e1834397e0e6ace1866ef69bb3da98e
Adithya S Narasinghe <adithyasnarasinghe@gmail.com>
2018-10-31 16:15:36 +0530

    Bug Fixes, Build Config Fixes & Polishing up the styling
    Polished up the styling on index & register pages
    Fixed the webpack config to support fonts & images
    Fixed the travis build config to support dev & prod builds

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,25 +1,28 @@
 import * as $ from 'jquery';
+import {MDCRipple} from '@material/ripple';
 
-$(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    if(scrollTop !== undefined){
-        if(scrollTop >= 40.79999923706055){
+$(window).scroll(function () {
+    let scrollTop = $(window).scrollTop();
+    if (scrollTop !== undefined) {
+        if (scrollTop >= 40.79999923706055) {
             $("#c-nav-bar").addClass('fixed');
             $("#c-nav-bar").removeClass('abs');
-        }else{
+        } else {
             $("#c-nav-bar").removeClass('fixed');
             $("#c-nav-bar").addClass('abs');
         }
     }
 });
 
-$(document).ready(function(){
-    $("#nav-mobile-res-btn").click(function(){
+$(document).ready(function () {
+    $("#nav-mobile-res-btn").click(function () {
         $('#nav-list').slideToggle(300);
-        if($("#c-nav-bar").hasClass('mob-res-on')){
+        if ($("#c-nav-bar").hasClass('mob-res-on')) {
             $("#c-nav-bar").removeClass('mob-res-on');
-        }else{
+        } else {
             $("#c-nav-bar").addClass('mob-res-on');
         }
     });
+    let buttonRipple = new MDCRipple(document.querySelector('.mdc-button')!);
 });
+
---
+++ webpack.prod.js
@@ -0,0 +1,66 @@
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),
+        compress: true,
+        port: 9000
+    },
+    entry: ['./src/styles/main.scss', './src/scripts/app.ts'],
+    output: {
+        filename: '[name].min.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].min.css',
+                        },
+                    },
+                    {loader: 'extract-loader'},
+                    {loader: 'css-loader'},
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()],
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: ['./node_modules'],
+                        },
+                    }],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {context: 'src', from: '**/*.html'},
+            {from: 'src/**/*.ttf', to: 'fonts/', flatten: true},
+            {from: 'src/**/*.png', to: 'img/', flatten: true},
+            {from: 'src/**/*.svg', to: 'img/', flatten: true},
+            {from: 'src/CNAME', to: 'CNAME', toType: 'file'},
+            {from: 'src/LICENSE', flatten: true},
+            {from: 'src/README.md', flatten: true}
+        ]),
+        new MinifyPlugin(),
+        new OptimizeCssAssetsPlugin()
+    ]
+};
